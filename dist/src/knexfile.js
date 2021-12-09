0eaecc7b5600fd6825b0b06f5f23cecf809fb169
kelvin santos <kesantos@paschoalotto.com.br>
2021-12-09 17:23:14 -0300

    erro de vscode

---
+++ dist/src/knexfile.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    development: {
+        client: 'mysql2',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'root',
+            database: 'aldisel_db',
+            port: 3306
+        }
+    }
+};
